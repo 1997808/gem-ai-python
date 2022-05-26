6a298d3e64a57c52ea10f381761137122f06337a
1997808 <khanhhuyle22@gmail.com>
2022-05-26 14:37:54 +0700

    model 2.59 acc

--- model_keras.py
+++ model_keras.py
@@ -14,24 +14,27 @@ from keras.layers import Dense
 
 # Load the dataset in a dataframe object and include only four features as mentioned
 url = "http://localhost:5000/api/train-data"
-df = pd.read_csv(url, header=None)
+file = '1653549851017.csv'
+df = pd.read_csv(file, low_memory=False)
 
-dataset = loadtxt(df)
+# dataset = loadtxt(df)
 data = df.astype('float32')
-print(data)
+print(data.shape)
 # split into input (X) and output (y) variables
-X = dataset[:,0:436]
-y = dataset[:,436]
-
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+cols=X.shape[1]
+print(y)
 # define the keras model
 model = Sequential()
-model.add(Dense(12, input_dim=446, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(cols, input_shape=(cols,), activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='relu'))
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # fit the keras model on the dataset
-model.fit(X, y, epochs=50, batch_size=10)
+model.fit(X, y, epochs=30, batch_size=10, validation_split=0.1)
 # evaluate the keras model
 _, accuracy = model.evaluate(X, y)
 print('Accuracy: %.2f' % (accuracy*100))
